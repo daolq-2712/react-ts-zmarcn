2e87f64d9beabb4cc063dd6e8fe640938b886f0b
daolq-2712 <le.quang.dao@sun-asterisk.com>
2022-08-01 16:11:29 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,14 +5,19 @@ import { Button } from './component/Button';
 import { List } from './component/List';
 
 export default function App() {
+  // Add
   const [todos, setTodos] = React.useState([]);
   const value = React.useRef(null);
+
+  // Search
+  const valueSearch = React.useRef(null);
+  const [todoResults, setTodoResults] = React.useState([]);
 
   const handleTexInputChange = (event) => {
     value.current = event.target.value;
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmitAdd = (event) => {
     event.preventDefault();
 
     if (value.current == '') {
@@ -23,14 +28,35 @@ export default function App() {
     setTodos(newTodos);
 
     value.current = '';
-    // event.target.reset();
+    event.target.reset();
   };
 
   const handleReset = () => {
     value.current = '';
   };
 
-  const handleItemDelete = (index) => {
+  const handleTexInputSearchChange = (event) => {
+    value.current = event.target.value;
+  };
+
+  const handleSubmitSearch = (event) => {
+    event.preventDefault();
+
+    if (value.current == '') {
+      return;
+    }
+
+    console.log(todos);
+    const newTodos = todos.filter((item) => item.includes(valueSearch.current));
+
+    setTodoResults(newTodos);
+
+    valueSearch.current = '';
+    event.target.reset();
+  };
+
+  // Item
+  const handleItemDeleteItem = (index) => {
     const newTodos = todos.filter((_, itemIndex) => itemIndex !== index);
 
     setTodos(newTodos);
@@ -38,18 +64,43 @@ export default function App() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitAdd}>
         <Input
           placeHolder="Type your todo"
           ref={value}
           type="text"
           onChange={handleTexInputChange}
         />
-        <Button type="submit" name="Submit" onClick={handleSubmit} />
+        <Button type="submit" name="Submit" />
         <Button type="reset" name="Reset" onClick={handleReset} />
       </form>
+      <h3>List Todo</h3>
+
       <div>
-        <List items={todos} onItemClick={(index) => handleItemDelete(index)} />
+        <List
+          items={todos}
+          onItemClick={(index) => handleItemDeleteItem(index)}
+        />
+      </div>
+
+      <hr></hr>
+
+      <form onSubmit={handleSubmitSearch}>
+        <Input
+          ref={valueSearch}
+          type="search"
+          onChange={handleTexInputSearchChange}
+          placeholder={`Search`}
+        />
+        <Button type="submit" name="Search" />
+      </form>
+
+      <h3>List Result</h3>
+      <div>
+        <List
+          items={todoResults}
+          onItemClick={(index) => handleItemDeleteItem(index)}
+        />
       </div>
     </div>
   );
